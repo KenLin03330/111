e5a1bf6fb6d28f6ac288ca706732f9118ab36791
KenLin03330 <a0932511985@gmail.com>
2023-12-05 11:42:18 +0800

    make it better

--- index.py
+++ index.py
@@ -16,39 +16,36 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    homepage = "<h1>林浚承Python1127網頁</h1>"
+    homepage = "<h1>浚承Python網頁1205</h1>"
     homepage += "<a href=/mis>MIS</a><br>"
     homepage += "<a href=/today>顯示日期時間</a><br>"
-    homepage += "<a href=/welcome?nick=tcyang>傳送使用者暱稱</a><br>"
-    homepage += "<a href=/about>林浚承簡介網頁</a><br>"
-    homepage += "<a href=/account>網頁表單輸入帳密傳值</a><br>"
-    homepage += "<br><a href=/read>人選之人演員查詢</a><br>"
-    homepage += "<br><a href=/books>圖文精選</a><br>"
-    homepage += "<br><a href=/query>圖文搜尋</a><br>"
-    homepage += "<br><a href=/spider>網路爬蟲抓取子青</a><br>"
+    homepage += "<a href=/welcome?nick=林浚承>傳送使用者暱稱</a><br>"
+    homepage += "<a href=/sweetpotato>浚承簡介網頁</a><br>"
+    homepage += "<a href=/account>輸入帳密傳值</a><br>"
+    homepage += "<a href=/read>人選之人演員查詢</a><br><br>"
+    homepage += "<a href=/books>精選圖書列表</a><br>"
+    homepage += "<a href=/query>書名查詢</a><br><br>"
+    homepage += "<a href=/spider>網路爬蟲抓取子青老師課程</a><br>"
     homepage += "<br><a href=/movie>讀取開眼電影即將上映影片，寫入Firestore</a><br>"
     homepage += "<br><a href=/searchQ>讀取開眼電影即將上映影片</a><br>"
+    homepage += "<br><a href=/movie_rate>讀取開眼電影即將上映影片(含分級資訊)，寫入Firestore</a><br>"
     return homepage
+
 
 @app.route("/mis")
 def course():
     return "<h1>資訊管理導論</h1>"
-
 @app.route("/today")
 def today():
     now = datetime.now()
     return render_template("today.html", datetime = str(now))
-
-@app.route("/about")
-def about():
-    now = datetime.now()
-    return render_template("about.html")
-
+@app.route("/sweetpotato")
+def sweetpotato():
+    return render_template("sweetpotato.html")
 @app.route("/welcome", methods=["GET", "POST"])
 def welcome():
     user = request.values.get("nick")
     return render_template("welcome.html", name=user)
-
 @app.route("/account", methods=["GET", "POST"])
 def account():
     if request.method == "POST":
@@ -62,24 +59,26 @@ def account():
 @app.route("/read")
 def read():
     Result = ""
-    collection_ref = db.collection("人選之人-造浪者")
-    docs = collection_ref.order_by("birth", direction=firestore.Query.DESCENDING).get()
-    for doc in docs:
-        x = doc.to_dict()
-        Result += "演員：{},劇中角色是{},出生於{}".format(x["name"],x["role"],x["birth"]) + "<br>"
+    db = firestore.client()
+    collection_ref = db.collection("人選之人─造浪者")    
+    docs = collection_ref.get()    
+    for doc in docs: 
+        X = doc.to_dict()        
+        Result += "演員：{},劇中角色是{},出生於{}".format(X["name"], X["role"], X["birth"]) + "<br>"    
     return Result
 
 @app.route("/books")
 def books():
     Result = ""
-    collection_ref = db.collection("圖書精選")
-    docs = collection_ref.order_by("anniversary").get()
-    for doc in docs: 
+    db = firestore.client()
+    collection_ref = db.collection("圖書精選")    
+    docs = collection_ref.get()
+    for doc in docs:
         bk = doc.to_dict()
         Result += "書名：<a href=" + bk["url"] + ">" + bk["title"] + "</a><br>"
         Result += "作者：" + bk["author"] + "<br>"
         Result += str(bk["anniversary"]) + "週年紀念版<br>"
-        Result += "<img src=" + bk["cover"] + "></img><br><br>"
+        Result += "<img src=" + bk["cover"] + "> </img><br><br>"
     return Result
 
 @app.route("/query", methods=["GET", "POST"])
@@ -92,30 +91,29 @@ def query():
         db = firestore.client()
         collection_ref = db.collection("圖書精選")
         docs = collection_ref.get()
-        for doc in docs: 
+        for doc in docs:
             bk = doc.to_dict()
-            if keyword in  bk["title"]:
+            if keyword in bk["title"]:
                 Result += "書名：<a href=" + bk["url"] + ">" + bk["title"] + "</a><br>"
                 Result += "作者：" + bk["author"] + "<br>"
                 Result += str(bk["anniversary"]) + "週年紀念版<br>"
-                Result += "<img src=" + bk["cover"] + "></img><br><br>"
+                Result += "<img src=" + bk["cover"] + "> </img><br><br>"
         return Result
     else:
-        return render_template("20231111.html")
+        return render_template("searchbooks.html")
 
 @app.route("/spider")
 def spider():
     url = "https://www1.pu.edu.tw/~tcyang/course.html"
     Data = requests.get(url)
     Data.encoding = "utf-8"
-
     sp = BeautifulSoup(Data.text, "html.parser")
     result=sp.select(".team-box")
     info = ""
     for x in result:
-        info += "<a href=" + x.find("a").get("href") + ">" + x.text + "<br>"
+        info += "<a href=" + x.find("a").get("href") + ">" + x.text + "</a><br>"
         info += x.find("a").get("href") + "<br><br>"
-    return info  
+    return info    
 
 @app.route("/movie")
 def movie():
@@ -169,6 +167,60 @@ def searchQ():
         return info
     else:  
         return render_template("movie.html")
+@app.route("/movie_rate")
+def movie_rate():
+    url = "http://www.atmovies.com.tw/movie/next/"
+    Data = requests.get(url)
+    Data.encoding = "utf-8"
+    sp = BeautifulSoup(Data.text, "html.parser")
+    result=sp.select(".filmListAllX li")
+    lastUpdate = sp.find(class_="smaller09").text[5:]
+
+    for x in result:
+        picture = x.find("img").get("src").replace(" ", "")
+        title = x.find("img").get("alt")    
+        movie_id = x.find("div", class_="filmtitle").find("a").get("href").replace("/", "").replace("movie", "")
+        hyperlink = "http://www.atmovies.com.tw" + x.find("a").get("href")
+
+        t = x.find(class_="runtime").text
+        showDate = t[5:15]
+
+        showLength = ""
+        if "片長" in t:
+            t1 = t.find("片長")
+            t2 = t.find("分")
+            showLength = t[t1+3:t2]
+
+        r = x.find(class_="runtime").find("img")
+        rate = ""
+        if r != None:
+            rr = r.get("src").replace("/images/cer_", "").replace(".gif", "")
+            if rr == "G":
+                rate = "普遍級"
+            elif rr == "P":
+                rate = "保護級"
+            elif rr == "F2":
+                rate = "輔12級"
+            elif rr == "F5":
+                rate = "輔15級"
+            else:
+                rate = "限制級"
+
+        doc = {
+            "title": title,
+            "picture": picture,
+            "hyperlink": hyperlink,
+            "showDate": showDate,
+            "showLength": showLength,
+            "rate": rate,
+            "lastUpdate": lastUpdate
+        }
+
+        db = firestore.client()
+        doc_ref = db.collection("電影含分級").document(movie_id)
+        doc_ref.set(doc)
+    return "近期上映電影已爬蟲及存檔完畢，網站最近更新日期為：" + lastUpdate
+
 
 
 
